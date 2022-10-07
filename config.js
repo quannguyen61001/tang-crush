84f4b6168df3e9ab49c24e2bb1ab9a1de91d5fab
quannguyen61001 <83213851+quannguyen61001@users.noreply.github.com>
2022-10-07 16:41:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     introTitle: 'Baeeeee à!',
-    introDesc: `Hôm nay là sinh nhật của Quách Lem nhà mình. 
-    Do cách nhau tận 350km "khók" 😢 
-    Nên Wannguyennn có món quà nhỏ muốn tặng cho Quách Lem nèeeeeeeee 🎂 
-    Hi vọng Quách Lem sẽ thích món quà nhỏ này của Wannguyennn nhaaaaaa ♥️`,
-    btnIntro: 'Sanh nhật dui dẻ 🔅',
-    title: 'Happy Birthday to Quách Lemmmm 😙',
-    desc: 'Mừng sinh nhật tuổi 21 của Bae nhaaaaa 🍭 ',
-    btnYes: 'Bae sinh nhật tuổi 21 dui nhaaaaaa <33',
+    introDesc: `Hôm nay là sinh nhật của Dĩm Dĩm nhà mình. 
+    Do cách nhau tận 12km "khók" 😢 
+    Nên Wannguyennn có món quà nhỏ muốn tặng cho Dĩm Dĩm nèeeeeeeee 🎂 
+    Hi vọng Dĩm Dĩm sẽ thích món quà nhỏ này của Wannguyennn nhaaaaaa ♥️`,
+    btnIntro: 'Sanh nhật dui dẻ nha em iuuuuuu 🔅',
+    title: 'Happy Birthday to bé iu nhà mìnhhhhhh 😙',
+    desc: 'Mừng sinh nhật tuổi 23 của Bae nhaaaaa 🍭 ',
+    btnYes: 'Bae sinh nhật tuổi 23 dui dẻeeeeeeeee nhaaaaaa <33',
     btnNo: 'Còn nữa á bấm nút màu đỏ á nha Baeeee :3',
-    question:'Lem hãy bấm gì đó đi gì cũng được  ❣️',
+    question:'Bé iu bấm gì đi cũng được  ❣️',
     btnReply: 'Gửi cho Wannguyennn nèeeeeeeee <3',
-    reply: 'Hi vọng tuổi 21 của Lemmm có thật nhiều niềm vui <33333333 Và sớm có được bé Wann nhaaaaaaaa Baeee 😜',
-    mess: 'Bấm vào đây để gửi nè 🥰. Hi vọng tương lai mình sẽ có nhau nheeeee 😘😘',
+    reply: 'Hi vọng tuổi 23 của công túaaaaa có thật nhiều niềm vui <33333333 Và iu thương bé Wannnn nhiều hơn nha Baeee 😜',
+    mess: 'Bấm vào đây để gửi nè 🥰. Hi vọng tụi mình sẽ ăn thật nhiều cái sinh nhật cùng nhau nữa nheeeee 😘😘',
     messDesc: 'Love you baeeeeee.',
     btnAccept: 'Okiiiii lun 💖',
     messLink: 'https://www.instagram.com/wannguyennn/' //link mess của các bạn. VD: https://www.instagram.com/direct/t/340282366841710300949128522297756336238
